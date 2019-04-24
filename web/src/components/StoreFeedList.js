eaa4cde572bf4cb1c238424bb237daf6130d989a
lotosbin <lotosbin@gmail.com>
2019-04-24 12:20:04 +0800

    feat: subscribe add title

--- web/src/components/StoreFeedList.js
+++ web/src/components/StoreFeedList.js
@@ -4,6 +4,6 @@ import styles from './StoreFeedList.module.css'
 
 const StoreFeedList = ({data, subscribeToNewFeeds}) => {
     useEffect(() => subscribeToNewFeeds());
-    return data.map(it => <div className={styles.list_item} key={it.id}><Link to={`/store/feed/${it.id}`}>{it.title || it.link}</Link><span><a href={`${process.env.REACT_APP_XREAD_URL}#/feed/subscribe?url=${it.link}`} target='_blank'>订阅</a></span></div>)
+    return data.map(it => <div className={styles.list_item} key={it.id}><Link to={`/store/feed/${it.id}`}>{it.title || it.link}</Link><span><a href={`${process.env.REACT_APP_XREAD_URL}#/feed/subscribe?url=${it.link}&title=${it.title}`} target='_blank'>订阅</a></span></div>)
 };
 export default StoreFeedList;
